7b48c369bca32a7c0f48cee8f023022dbd3df2d1
Бруйко Евгения <bruyko.e@gmail.com>
2021-02-05 23:57:26 +0300

    Initial Commit

---
+++ Custom Index View/Custom_Index_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Index_ViewApp.swift
+//  Custom Index View
+//
+//  Created by Brubrusha on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Index_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
